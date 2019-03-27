d44e31c597eb15e0070ff3930ee6945690ce02b3
David Sanders <davesque@gmail.com>
2019-03-27 13:56:38 -0600

    Add convenience function parse_and_print

--- vyper_parser/cst.py
+++ vyper_parser/cst.py
@@ -106,11 +106,18 @@ def get_pretty_lark_repr(node: LarkNode, indent: int = 0) -> str:
     return s
 
 
-def pretty_print_lark(node: LarkNode):
+def pretty_print_lark(node: LarkNode) -> None:
     """
     Pretty prints the given lark node to stdout.
     """
     print(get_pretty_lark_repr(node))
+
+
+def parse_and_print(source_code: str) -> None:
+    """
+    Parses the given source code and pretty prints the parse tree.
+    """
+    pretty_print_lark(parse_python(source_code))
 
 
 def get_num_stmts(tree: Tree) -> int:
